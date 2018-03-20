d254094c6811fb9e91b936884259c0fe3ba76508
Shreef Hagazi <shagazi@e1z2r4p22.42.us.org>
2018-03-19 18:43:52 -0700

    ASdfA

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: shagazi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/26 15:56:12 by shagazi           #+#    #+#             */
-/*   Updated: 2018/03/19 18:41:50 by shagazi          ###   ########.fr       */
+/*   Updated: 2018/03/19 18:43:14 by shagazi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,13 @@ char	*ft_strstr(const char *haystack, const char *needle)
 	i = 0;
 	j = 0;
 	if (needle[i] == '\0')
-		return (haystack);
+		return ((char*)haystack);
 	while (haystack[i] != '\0')
 	{
 		if (haystack[i] == needle[j])
 		{
 			if (ft_strcmp(haystack + i, needle) == 0)
-					return (haystack + i);
+					return ((char *)haystack + i);
 			else
 				j = 0;
 		}
